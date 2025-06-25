498701eb5e6669e0a9200be15d9d3bdeb3c675c1
SethM28 <sethmartey360@gmail.com>
2025-06-25 04:11:22 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/orders', (req, res) => {
+  fs.readFile('orders.json', 'utf8', (err, data) => {
+    if (err) return res.status(500).send('Error reading orders');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
